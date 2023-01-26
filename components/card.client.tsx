89e75526e1737f6a9151eab3776a4cc91940bb96
Ankita Kulkarni <kulkarni.ankita09@gmail.com>
2023-01-26 12:21:27 -0500

    Day 5: Date inside Shared component

--- components/card.client.tsx
+++ components/card.client.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { CardType } from 'types/types';
+import DisplayDate from './date';
 
 export default function Card({ title, description, date }: CardType) {
   const handleOnClick = () => {};
@@ -7,7 +8,7 @@ export default function Card({ title, description, date }: CardType) {
     <div>
       <h2> {title}</h2>
       <p>{description}</p>
-      <time dateTime={date}>{date}</time>
+      <DisplayDate dateString={date} />
       <div>
         <button onClick={handleOnClick}>Go</button>
       </div>
